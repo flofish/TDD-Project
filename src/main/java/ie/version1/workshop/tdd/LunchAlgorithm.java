32b95fc5c3842851bd7e4a93c23979f0f94c8f3e
flofish <l.fco@hotmail.com>
2017-03-24 09:07:50 +0000

    Refactor: Each member has lunch with x previous pals max
    Introducing "eligibility" factor and algorithm treshold. Right now the
    40x5 problem only works with a min treshold of 7. Target treshold is 2.

--- src/main/java/ie/version1/workshop/tdd/LunchAlgorithm.java
+++ src/main/java/ie/version1/workshop/tdd/LunchAlgorithm.java
@@ -21,12 +21,12 @@ public class LunchAlgorithm {
 	}
 	
 	public List<Lunch> getLunches() {
-		Map<Member, Map<Member, Integer>> controlMap = newControlMap(members);
+		Map<Member, Map<Member, Integer>> controlMap = newControlMap(leaders, members);
 		List<Lunch> lunchList = new ArrayList<Lunch>();
 		for(int i=0; i < leaders.size(); i++){
 			Lunch lunch = new Lunch();
 			populateTableLeaders(lunch, leaders);
-			populateTableMembers(lunch, members, controlMap);
+			populateTableMembers(lunch, members, treshold, controlMap);
 			lunchList.add(lunch);
 		}
 		return lunchList;
@@ -34,56 +34,93 @@ public class LunchAlgorithm {
 	
 	private void populateTableLeaders(Lunch lunch, List<Leader> lunchLeaders){
 		for(Leader lunchLeader:lunchLeaders){
-			lunch.getTables().put(lunchLeader, new ArrayList<Member>());
+			List<Member> table = new ArrayList<>();
+			table.add(lunchLeader);
+			lunch.getTables().put(lunchLeader, table);
 		}
 	}
 	
-	private void populateTableMembers(Lunch lunch, List<Member> lunchMembers, Map<Member, Map<Member, Integer>> controlMap){
+	private void populateTableMembers(Lunch lunch, List<Member> lunchMembers, int treshold, Map<Member, Map<Member, Integer>> controlMap){
 		Set<Member> membersAlreadyInLunch = new HashSet<>();
-		int expectedTableSize = lunchMembers.size() / lunch.getTables().size();
+		int expectedTableSize = lunchMembers.size() / lunch.getTables().size() + 1;
 		for(Entry<Leader,List<Member>> table:lunch.getTables().entrySet()){
-			for(Member lunchMember:lunchMembers){
-				if(!membersAlreadyInLunch.contains(lunchMember)){
-					if(canSitOnTable(lunchMember, table, controlMap)){
-						sitsOnTable(lunchMember, table, controlMap);
-						membersAlreadyInLunch.add(lunchMember);
+			while(table.getValue().size() < expectedTableSize){
+				int eligibility = -1;
+				Member eligibleMember = null;
+				for(Member lunchMember:lunchMembers){
+					if(!membersAlreadyInLunch.contains(lunchMember)){
+						int newEligibility = calculateEligibility(lunchMember, table, treshold, controlMap);
+						if(newEligibility > eligibility){
+							eligibility = newEligibility;
+							eligibleMember = lunchMember;
+						}
 					}
 				}
-				if(table.getValue().size() >= expectedTableSize){
-					break;
+				if(eligibleMember != null){
+					sitsOnTable(eligibleMember, table, controlMap);
+					membersAlreadyInLunch.add(eligibleMember);
+				} else {
+					throw new IllegalStateException("No more eligible members left.");
 				}
 			}
 		}
 	}
 	
-	private Map<Member, Map<Member, Integer>> newControlMap(List<Member> lunchMembers){
+	private Map<Member, Map<Member, Integer>> newControlMap(List<Leader> lunchLeaders, List<Member> lunchMembers){
 		Map<Member, Map<Member, Integer>> controlMap = new HashMap<>();
+		for(Leader leader:lunchLeaders){
+			controlMap.put(leader, new HashMap<Member, Integer>());
+		}
 		for(Member member:lunchMembers){
 			controlMap.put(member, new HashMap<Member, Integer>());
 		}
 		return controlMap;
 	}
-	
-	private boolean canSitOnTable(Member member, Entry<Leader,List<Member>> table, Map<Member, Map<Member, Integer>> controlMap){
+
+	private int calculateEligibility(Member member, Entry<Leader,List<Member>> table, int treshold, Map<Member, Map<Member, Integer>> controlMap){
 		
-		// can't have lunch with same pal more than twice
-		for(Member lunchPal:table.getValue()){
-			if(controlMap.get(member).keySet().contains(lunchPal)
-				&& controlMap.get(member).get(lunchPal) >= 2){
-				return false;
-			}
-			
+		int eligibility = Integer.MAX_VALUE;
+		
+		List<Member> previousPals = getPreviousPals(member, table, 0, controlMap);
+		
+		// can't have lunch with more that x previous pals
+		if(previousPals.size() > treshold){
+			return -1;
 		}
 		
-		return true;
+		for(Member previousPal:previousPals){
+			// checks that if seated, previous pals won't exceed the x previous pals rule
+			if(getPreviousPals(previousPal, table, 1, controlMap).size() > (treshold - 1)){
+				return -1;
+			}
+		}
+		
+		return eligibility;
+	}
+	
+	private List<Member> getPreviousPals(Member member, Entry<Leader, List<Member>> table, int treshold, Map<Member, Map<Member, Integer>> controlMap){
+		List<Member> previousPals = new ArrayList<>();
+		for(Member pal:table.getValue()){
+			if(controlMap.get(member).get(pal) != null
+					&& controlMap.get(member).get(pal) > treshold){	
+				previousPals.add(pal);
+			}
+		}
+		return previousPals;
 	}
 	
 	private void sitsOnTable(Member member, Entry<Leader,List<Member>> table, Map<Member, Map<Member, Integer>> controlMap){
 		
 		// adds pals to control
 		for(Member lunchPal:table.getValue()){
-			Integer count = controlMap.get(member).get(lunchPal);
-			controlMap.get(member).put(lunchPal, count == null ? 1 : count + 1);
+			if(!lunchPal.equals(member)){
+				// adds 1 to member count
+				Integer count = controlMap.get(member).get(lunchPal);
+				controlMap.get(member).put(lunchPal, count == null ? 1 : count + 1);
+				// adds 1 to pal count
+				count = controlMap.get(lunchPal).get(member);
+				controlMap.get(lunchPal).put(member, count == null ? 1 : count + 1);
+			}
 		}
 		
 		// adds member to table
